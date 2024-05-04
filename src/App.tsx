995e48bd3b8836ee7ebbf7e049d71a205f058b43
rashed <rashedpaykary@gmail.com>
2024-05-04 22:03:00 +0330

    feat: add Layout Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import Card from "./components/Card/Card";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div className="flex flex-wrap gap-3">
-      <Card
-        title="Node JS"
-        discription="از بهترین انتخاب‌ها برای سرمایه‌گذاری روی مهارت‌"
-        info=" 28 هفته | 300 ساعت آموزش"
-        action={() => alert("Node")}
-      />
-    </div>
+    <Layout>
+      <div className="flex flex-wrap gap-3">
+        <Card
+          title="Node JS"
+          discription="از بهترین انتخاب‌ها برای سرمایه‌گذاری روی مهارت‌"
+          info=" 28 هفته | 300 ساعت آموزش"
+          action={() => alert("Node")}
+        />
+        <Card
+          title="Node JS"
+          discription="از بهترین انتخاب‌ها برای سرمایه‌گذاری روی مهارت‌"
+          info=" 28 هفته | 300 ساعت آموزش"
+          action={() => alert("Node")}
+        />
+        <Card
+          title="Node JS"
+          discription="از بهترین انتخاب‌ها برای سرمایه‌گذاری روی مهارت‌"
+          info=" 28 هفته | 300 ساعت آموزش"
+          action={() => alert("Node")}
+        />
+      </div>
+    </Layout>
   );
 }
 
