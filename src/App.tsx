eeff9c7041e51943b4ed2bfae0b2297d9e85696d
rashed <rashedpaykary@gmail.com>
2024-05-04 14:57:46 +0330

    feat: create A sample component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import image from "./assets/node.png";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex flex-wrap gap-3">
+      <div className="border-2 border-solid border-white p-3 rounded-md shadow-[0_2px_10px_0_#999999] flex flex-col justify-between items-center m-2 basis-64 grow">
+        <h2 className="font-bold text-2xl">Node Js</h2>
+        <img src={image} alt="" width={"25%"} />
+        <p className="font-normal text-md text-justify leading-loose">
+          Node.js از بهترین انتخاب‌ها برای سرمایه‌گذاری روی مهارت‌هاست. اگه به
+          دنبال یادگیری یه مهارت ‌آینده‌دار هستی، تا رسیدن به اون فقط یه بوت‌کمپ
+          فاصله داری!
+        </p>
+        <div className="text-xl font-medium px-2 py-3 my-3 mx-0 border-t-2 border- border-[#e5e5e5] border-b-2 text-center w-full">
+          28 هفته | 300 ساعت آموزش
+        </div>
+        <button className="rounded-md px-3 py-3 font-normal text-white bg-purple-700 text-md w-full">
+          اطلاعات بیشتر
+        </button>
+      </div>
+      <div className="border-2 border-solid border-white p-3 rounded-md shadow-[0_2px_10px_0_#999999] flex flex-col justify-between items-center m-2 basis-64 grow">
+        <h2 className="font-bold text-2xl">Node Js</h2>
+        <img src={image} alt="" width={"25%"} />
+        <p className="font-normal text-md text-justify leading-loose">
+          Node.js از بهترین انتخاب‌ها برای سرمایه‌گذاری روی مهارت‌هاست. اگه به
+          دنبال یادگیری یه مهارت ‌آینده‌دار هستی، تا رسیدن به اون فقط یه بوت‌کمپ
+          فاصله داری!
+        </p>
+        <div className="text-xl font-medium px-2 py-3 my-3 mx-0 border-t-2 border- border-[#e5e5e5] border-b-2 text-center w-full">
+          28 هفته | 300 ساعت آموزش
+        </div>
+        <button className="rounded-md px-3 py-3 font-normal text-white bg-purple-700 text-md w-full">
+          اطلاعات بیشتر
+        </button>
+      </div>
+      <div className="border-2 border-solid border-white p-3 rounded-md shadow-[0_2px_10px_0_#999999] flex flex-col justify-between items-center m-2 basis-64 grow">
+        <h2 className="font-bold text-2xl">Node Js</h2>
+        <img src={image} alt="" width={"25%"} />
+        <p className="font-normal text-md text-justify leading-loose">
+          Node.js از بهترین انتخاب‌ها برای سرمایه‌گذاری روی مهارت‌هاست. اگه به
+          دنبال یادگیری یه مهارت ‌آینده‌دار هستی، تا رسیدن به اون فقط یه بوت‌کمپ
+          فاصله داری!
+        </p>
+        <div className="text-xl font-medium px-2 py-3 my-3 mx-0 border-t-2 border- border-[#e5e5e5] border-b-2 text-center w-full">
+          28 هفته | 300 ساعت آموزش
+        </div>
+        <button className="rounded-md px-3 py-3 font-normal text-white bg-purple-700 text-md w-full">
+          اطلاعات بیشتر
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
